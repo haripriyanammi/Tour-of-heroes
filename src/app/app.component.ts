0a9a24edfd8885cc23fd58d449631b28f5315930
haripriyanammi <21d41a05h6@gmail.com>
2024-12-20 00:15:39 +0530

    Add modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Tour of Heroes';
 }
