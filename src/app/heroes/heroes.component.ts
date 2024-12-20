27632d99a6cfb1e122ace2fd628470e060fb541d
haripriyanammi <21d41a05h6@gmail.com>
2024-12-20 14:45:36 +0530

    list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';  // Import CommonModule
-import { FormsModule } from '@angular/forms';  // Import FormsModule
+import {Component} from '@angular/core';
+import {
+  NgIf,
+  NgFor,
+  UpperCasePipe,
+} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
+  standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  standalone: true,  // Mark the component as standalone
-  imports: [CommonModule, FormsModule]  // Add FormsModule here
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+  ],
 })
+
 export class HeroesComponent {
-  hero = { id: 1, name: 'Windstorm' };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
